8dc096f19933596f4d997b596e25be7ea1ea7d08
Cole Nelson <ctnelson2@wisc.edu>
2026-04-20 21:22:49 -0500

    disclaimer

--- solution/src/tools/getComments.js
+++ solution/src/tools/getComments.js
@@ -1,6 +1,6 @@
 export default async function getComments(args) {
     const url = (args.n !== null && args.n > 0)
-        ? `https://cs571api.cs.wisc.edu/rest/s26/ice/comments?num=${args.n}`
+        ? `https://cs571api.cs.wisc.edu/rest/s26/ice/comments?num=${args.n}` // `num` doesn't exist on hw11; do client-side filtering :)
         : "https://cs571api.cs.wisc.edu/rest/s26/ice/comments";
     const resp = await fetch(url, {
         headers: { "X-CS571-ID": CS571.getBadgerId() }
